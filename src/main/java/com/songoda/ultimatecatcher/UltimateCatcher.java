69eeeece2badb9a55a1b535c6a76e9b863d155c4
Brianna <bri@songoda.com>
2021-02-25 11:59:54 -0600

    Made adjustments for previous PR.

--- src/main/java/com/songoda/ultimatecatcher/UltimateCatcher.java
+++ src/main/java/com/songoda/ultimatecatcher/UltimateCatcher.java
@@ -15,7 +15,7 @@ import com.songoda.ultimatecatcher.commands.CommandReload;
 import com.songoda.ultimatecatcher.commands.CommandSettings;
 import com.songoda.ultimatecatcher.egg.CEgg;
 import com.songoda.ultimatecatcher.egg.EggManager;
-import com.songoda.ultimatecatcher.hook.ExternalHooks;
+import com.songoda.ultimatecatcher.hook.ExternalHookManager;
 import com.songoda.ultimatecatcher.listeners.DispenserListeners;
 import com.songoda.ultimatecatcher.listeners.EntityListeners;
 import com.songoda.ultimatecatcher.listeners.EntityPickupListeners;
@@ -44,7 +44,7 @@ public class UltimateCatcher extends SongodaPlugin {
     private EggManager eggManager;
     private CommandManager commandManager;
     private EntityListeners entityListeners;
-    private ExternalHooks externalHooks;
+    private ExternalHookManager externalHookManager;
     
     private final Set<NamespacedKey> registeredRecipes = new HashSet<>();
 
@@ -76,7 +76,7 @@ public class UltimateCatcher extends SongodaPlugin {
         EntityStackerManager.load();
         
         // Load hook manager
-        externalHooks = new ExternalHooks(this);
+        externalHookManager = new ExternalHookManager(this);
 
         // Register commands
         this.commandManager = new CommandManager(this);
@@ -217,8 +217,8 @@ public class UltimateCatcher extends SongodaPlugin {
         return entityListeners;
     }
     
-    public ExternalHooks getExternalHooks() {
-    	return externalHooks;
+    public ExternalHookManager getExternalHookManager() {
+    	return externalHookManager;
     }
 
     public Config getMobConfig() {
--- src/main/java/com/songoda/ultimatecatcher/hook/IExternalHook.java
+++
@@ -1,9 +0,0 @@
-package com.songoda.ultimatecatcher.hook;
-
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-
-public interface IExternalHook
-{
-	public boolean shouldStopCapture(Player attemptingPlayer, Entity entityToCapture);
-}
